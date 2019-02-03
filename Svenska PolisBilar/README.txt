7a92b0d4c9aca773a656df4d568abb43d877bd9c
Ibbe <47153874+imme215@users.noreply.github.com>
2019-02-03 17:49:44 +0100

    Add files via upload

---
+++ Svenska PolisBilar/README.txt
@@ -0,0 +1,5 @@
+Lägg filerna i (resources)
+
+I server.cfg
+start V90
+start V90-Unmarked
---
+++ Svenska PolisBilar/V90/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Made By Imme215
+
+files {
+    'vehicles.meta',
+	'handling.meta',
+	'visualsettings.dat'
+	
+}
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VISUALSETTINGS_FILE' 'visualsettings.dat'
